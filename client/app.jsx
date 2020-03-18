d07d402667c8a8c32b97d5d334c7cb0595514d80
Eric Liu <eric@mikes-MBP-3.attlocal.net>
2020-03-17 22:08:59 -0700

    Displaying and cycling AWS S3 photos

--- client/app.jsx
+++ client/app.jsx
@@ -7,21 +7,6 @@ import path from 'path';
 import Gallery from './Gallery.jsx';
 import Viewer from './Viewer.jsx';
 
-const testPlace = {
-  _id: 1,
-  name: 'test',
-  urls: [
-    'https://www.planetware.com/photos-large/F/eiffel-tower.jpg',
-    'https://s3.amazonaws.com/crowdriff-media/full/b2c44bc8bc8d231f2189ba380e64c5bfa1940a778bd9f5a4dfc617bfc04b79bd.jpg',
-    'https://www.history.com/.image/t_share/MTY1MTc1MTk3ODI0MDAxNjA5/topic-statue-of-liberty-gettyimages-960610006-promo.jpg',
-    'https://i5.walmartimages.ca/images/Enlarge/094/514/6000200094514.jpg',
-    'https://i5.walmartimages.com/asr/209bb8a0-30ab-46be-b38d-58c2feb93e4a_1.1a15fb5bcbecbadd4a45822a11bf6257.jpeg?odnHeight=450&odnWidth=450&odnBg=FFFFFF',
-    'https://i.ndtvimg.com/mt/cooks/2014-11/carrots.jpg',
-    'https://www.alimentarium.org/en/system/files/thumbnails/image/WEB-Carr%C3%A9-durian.png',
-    'https://cdn.vox-cdn.com/thumbor/TGJMIRrhzSrTu1oEHUCVrizhYn0=/1400x1400/filters:format(jpeg)/cdn.vox-cdn.com/uploads/chorus_asset/file/13689000/instagram_egg.jpg',
-    'https://cookieandkate.com/images/2018/05/baked-falafel-recipe.jpg']
-}
-
 class App extends React.Component {
   constructor(props) {
     super(props);
@@ -37,6 +22,7 @@ class App extends React.Component {
     this.clickHandler = this.clickHandler.bind(this);
     this.buttonHandler = this.buttonHandler.bind(this);
     this.keypressHandler = this.keypressHandler.bind(this);
+    this.advanceDisplay = this.advanceDisplay.bind(this);
   }
 
   // makes API call with random restaurant ID between 0-99
@@ -61,9 +47,9 @@ class App extends React.Component {
     event.preventDefault();
     let eventId = event.target.id;
     if (eventId === 'left-arrow') {
-      this.keypressHandler('left');
+      this.advanceDisplay('left');
     } else if (eventId === 'right-arrow') {
-      this.keypressHandler('right');
+      this.advanceDisplay('right');
     } else if (eventId === 'close-button') {
       this.setState({ showViewer: false });
     } else if (eventId === 'viewer-background') {
@@ -88,7 +74,7 @@ class App extends React.Component {
   render() {
     return (
       <div id="container">
-        <Gallery place={testPlace} clickHandler={this.clickHandler} />
+        <Gallery place={this.state.place} clickHandler={this.clickHandler} />
         <Viewer show={this.state.showViewer} place={this.state.place}
           currentIndex={this.state.currentIndex} buttonHandler={this.buttonHandler} />
       </div>
@@ -96,5 +82,5 @@ class App extends React.Component {
   }
 }
 
-// ReactDOM.render(<App />, document.getElementById('app'));
+ReactDOM.render(<App />, document.getElementById('app'));
 export default App;
